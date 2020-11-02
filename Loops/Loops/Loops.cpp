5356cc9f1752c3be1323cf2b21a805fd72b8b54d
Ruben F Andrade <rubenswork20@gmail.com>
2020-11-02 16:02:43 +0000

    test

---
+++ Loops/Loops/Loops.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i;
+	}
+
+	bool isgamerunning = true;
+
+	while (isgamerunning)
+	{
+
+	}
+
+	bool isplayeralive = true;
+
+	do
+	{
+
+	}
+	while (isplayeralive);
+
+	int numbers[5] = { 1, 2, 3, 4, 5 };
+
+	for (int i : numbers)
+	{
+		cout << i << endl;
+	}
+
+
+
+
+
+	system("pause");
+	return 0;
+}
